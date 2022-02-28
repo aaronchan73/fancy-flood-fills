a04f9e2d3b9beabc39024c1951788712b545fc42
aaronchan73 <chan.aaron73@gmail.com>
2022-02-27 20:51:05 -0800

    negativeColorPicker.cpp.
    Co-authored-by: Alvin Zhou <alvzhou@users.noreply.github.com>

--- imageTileColorPicker.cpp
+++ imageTileColorPicker.cpp
@@ -12,5 +12,13 @@ ImageTileColorPicker::ImageTileColorPicker(PNG& otherimage) {
 
 HSLAPixel ImageTileColorPicker::operator()(PixelPoint p) {
   // complete your implementation below
+  // img_main contains pixelpoint p parameter, calls imagetile
+  // x y x y img_main x = 2, img_other width = 2, 2 % 2 = 0
+  // w z w z  img main y = 2; img other height = 2; 2%2=0
+  // x o o o
+  // o o o o
+
+  // x y img_other 
+  // w z
   return *img_other.getPixel((p.x % (img_other.width())), (p.y % (img_other.height())));
 }
--- negativeColorPicker.cpp
+++ negativeColorPicker.cpp
@@ -16,9 +16,9 @@ HSLAPixel NegativeColorPicker::operator()(PixelPoint p)
 {
   // complete your implementation below
   HSLAPixel pixel;
-  pixel.h = p.color.h;
-  pixel.s = p.color.s;
-  pixel.l = 1 - p.color.l;
-  pixel.a = p.color.a;
+  pixel.h = fmod((img.getPixel(p.x, p.y)->h + 180), 360);
+  pixel.s = img.getPixel(p.x, p.y)->s;
+  pixel.l = 1 - img.getPixel(p.x, p.y)->l;
+  pixel.a = img.getPixel(p.x, p.y)->a;
   return pixel;
 }
